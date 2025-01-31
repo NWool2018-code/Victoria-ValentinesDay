14a58fde6ea6e174a571514bacb3508cba035108
NWool2018-code <nwool2018@gmail.com>
2025-01-31 10:37:41 -0500

    Add files via upload

--- src/Yes.tsx
+++ src/Yes.tsx
@@ -4,16 +4,16 @@ import './Yes.css';
 const Yes = () => {
   // Gifts inside the envelopes
   const messages = [
-    'You get massage this evening.',
-    'You can choose the movie we are watching.',
-    'I cook three course dinner.',
+    'Chocolate, flowers, and a meal reminscent of our first date',
+    "Romantic Dinner at Dom & Mia's Italian Restaurant",
+    'I cook three course dinner. (Flowers Included)',
   ];
 
   const envelopes = messages.map((message, index) => (
     <div key={index} className="flex flex-col items-center">
       <Envelope id={index}>{message}</Envelope>
       <p className="text-slate-100 mb-2 text-2xl">
-        {['For today', 'Anytime', 'Romantic time!'][index]}
+        {['February 2', 'February 14', 'Any Time!'][index]}
       </p>
     </div>
   ));
@@ -23,11 +23,10 @@ const Yes = () => {
         <div className="bg-black pt-14 pb-11 mx-4 md:mx-24 md:my-20 my-10 rounded-3xl shadow-2xl opacity-70 shadow-red-600">
           <div>
             <h1 className="text-slate-100 md:text-5xl text-3xl text-center red-shadow">
-              Happy Valentine's Day!
+              Happy Valentine's Day Victoria!
             </h1>
             <p className="text-slate-100 text-center mt-10 text-xl ">
-              This Valentines you will get tickets to use when you want to.
-              Remember you can only use them once, so choose wisely!
+              I love you and hope that we have wonderful memories this Valentine's Day and many more to come!
             </p>
           </div>
           <div className="envelopes-container flex justify-center flex-col xl:flex-row gap-4 mt-4">
